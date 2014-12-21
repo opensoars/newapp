679225aee8f31b3d7dff1a971542f8b0848baddd
unknown <sam_1700@hotmail.com>
2014-12-21 12:52:26 +0100

    Basic logging

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var Ezlog = require('Ezlog'),
+    log = new Ezlog(['[newapp]', 'yellow']),
+    logErr = new Ezlog(['[newapp]', 'yellow'], ['red']);
+
+
 var argv = process.argv;
 
 if(!argv[2])
+  return logErr('Please specify what to build, i.e: `newapp node`');
+
+
+var fs = require('fs');
+
